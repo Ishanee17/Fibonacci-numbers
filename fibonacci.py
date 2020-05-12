e344edd7ff05ae9b12a769f5e989773878555fd8
Ishanee17 <64854200+Ishanee17@users.noreply.github.com>
2020-05-12 13:32:29 +0530

    Update fibonacci.py
    used if/else to create a few changes so that it can display a single term.

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,21 @@
 #creating a new function named as fib(n)
 def fib(n):
-#creating an input    
-    n = int(input("Enter the number of terms in the series: "))
+    n = int(input("enter the the number of terms in the series: "))
     a=0
     b=1
-    print(a)
-    print(b)
+    if n==1:
+        print(a)
+    elif n==0:
+        print("no series")
+    elif n<0:
+        print("wrong data-only positive numbers")
+    else:
+        print(a)
+        print(b)
     for i in range(2,n):
-        c = a + b
-        a = b
-        b = c
+        c=a + b
+        a=b
+        b=c
         print(c)
+    
+fib(100)
